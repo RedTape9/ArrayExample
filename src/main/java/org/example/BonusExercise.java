5e24a0818560fde970516203c4a182140e0670ee
Sergej Jaudszims <redtape9688@gmail.com>
2023-11-01 12:58:45 +0100

    remove method

--- src/main/java/org/example/BonusExercise.java
+++ src/main/java/org/example/BonusExercise.java
@@ -7,19 +7,13 @@ public class BonusExercise {
         Scanner scn = new Scanner(System.in);
         String inputString = scn.nextLine();
 
-        printChar(inputString);
-    }
-
-    // einzelne Zeichen von inputSring ausgeben
-    public static void printChar(String inputString) {
         for (int i = 0; i < inputString.length(); i++) {
             System.out.println(inputString.charAt(i));
-
         }
     }
 }
 
-    
+
 
 
 
