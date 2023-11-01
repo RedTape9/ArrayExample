2349393243d424c8e777aaac347c197a2f3bf406
Sergej Jaudszims <redtape9688@gmail.com>
2023-11-01 14:12:56 +0100

    extract to methode, close Scanner

--- src/main/java/org/example/BonusExercise.java
+++ src/main/java/org/example/BonusExercise.java
@@ -1,12 +1,17 @@
 package org.example;
 
+
 import java.util.Scanner;
 
 public class BonusExercise {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         String inputString = scn.nextLine();
+        printAllChars(inputString);
+        scn.close();
+    }
 
+    private static void printAllChars(String inputString) {
         for (int i = 0; i < inputString.length(); i++) {
             System.out.println(inputString.charAt(i));
         }
